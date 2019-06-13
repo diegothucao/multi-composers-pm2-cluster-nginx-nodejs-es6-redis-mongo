7b60ffdcde11717bf9678fce49ee83b6a6bfd743
Thu CAO <cao.trung.thu@gmail.com>
2019-06-13 14:09:17 +0700

    update redis

--- src/app.js
+++ src/app.js
@@ -3,8 +3,10 @@ import express from 'express'
 import cors from 'cors'
 import { urlencoded, json } from 'body-parser'
 import dotenv from 'dotenv'
+import redisClient from './redis-client'
 
 dotenv.load()
+
 const app = express()
 app.use(urlencoded({ extended: true, limit: '500mb'}))
 app.use(json({ extended: true, limit: '500mb'}))
@@ -13,6 +15,19 @@ app.use(cors())
 app.get('/', (_, res) => {
   res.send('Diego Cao: Hello')
 });
+
+app.get('/store/:key', async (req, res) => {
+  const { key } = req.params;
+  const value = req.query;
+  await redisClient.setAsync(key, JSON.stringify(value));
+  return res.send('Success');
+})
+
+app.get('/:key', async (req, res) => {
+  const { key } = req.params;
+  const rawData = await redisClient.getAsync(key);
+  return res.json(JSON.parse(rawData));
+})
 
 let server = app.listen(process.env.PORT || 8080)
 server.setTimeout(500000)
---
+++ src/redis-client.js
@@ -0,0 +1,11 @@
+import { createClient } from 'redis'
+import { promisify } from 'util'
+
+const client = createClient(process.env.REDIS_URL);
+
+export default {
+  ...client,
+  getAsync: promisify(client.get).bind(client),
+  setAsync: promisify(client.set).bind(client),
+  keysAsync: promisify(client.keys).bind(client)
+}
