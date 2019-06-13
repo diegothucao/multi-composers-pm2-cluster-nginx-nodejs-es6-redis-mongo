471188b3dcd7a5d587c3d10593c5c22e6de7e38d
Diego CAO <cao.trung.thu@gmail.com>
2019-06-13 19:13:40 +0700

    update config

--- src/app.js
+++ src/app.js
@@ -14,19 +14,21 @@ app.use(cors())
 
 app.get('/', (_, res) => {
   res.send('Diego Cao: Hello')
-});
+})
 
+// set data to Redis
 app.get('/store/:key', async (req, res) => {
   const { key } = req.params;
   const value = req.query;
-  await redisClient.setAsync(key, JSON.stringify(value));
-  return res.send('Success');
+  await redisClient.setAsync(key, value)
+  return res.send('Success')
 })
 
+// get data from Redis 
 app.get('/:key', async (req, res) => {
   const { key } = req.params;
   const rawData = await redisClient.getAsync(key);
-  return res.json(JSON.parse(rawData));
+  return res.json(awData);
 })
 
 let server = app.listen(process.env.PORT || 8080)
